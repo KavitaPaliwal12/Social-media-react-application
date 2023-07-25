820a7820bac5e20493c7ddf835f67cca98c1630b
Kavita Paliwal <kavitapaliwal@Kavitas-MacBook-Pro.local>
2023-07-25 10:59:01 +0530

    Rightbar design finished

--- src/components/rightbar/Rightbar.jsx
+++ src/components/rightbar/Rightbar.jsx
@@ -3,7 +3,67 @@ import "./rightbar.css"
 export default function Rightbar() {
   return (
     <div className="rightbar"> 
-      rightbar
+        <div className="rightbarWrapper">
+          <div className="birthdayContainer">
+            <img src = "assets/gift.png" className="birthdayImg" alt=""/>
+            <span className="birthdatText"> 
+              <b>Pola Foaster</b> and <b>3 other friends </b>have a birthday today.
+            </span>
+          </div>
+          <img src = "assets/ad.png" className="rightbarAd" alt=""/>
+          <h4 className="rightbarTitle">Online Friends</h4>
+          <ul className="onlineFriendsList">
+            <li className="rightbarFriend">
+               <div className="rightbarProfileImgContainer">
+                  <img className="rightbarProfileImg" src="assets/person/3.jpeg" alt=""/>
+                  <span className="rightbarOnline"></span>
+               </div>
+               <span className="rightbarUsername"> Kavita P</span>
+            </li>
+            <li className="rightbarFriend">
+               <div className="rightbarProfileImgContainer">
+                  <img className="rightbarProfileImg" src="assets/person/3.jpeg" alt=""/>
+                  <span className="rightbarOnline"></span>
+               </div>
+               <span className="rightbarUsername"> Kavita P</span>
+            </li>
+            <li className="rightbarFriend">
+               <div className="rightbarProfileImgContainer">
+                  <img className="rightbarProfileImg" src="assets/person/3.jpeg" alt=""/>
+                  <span className="rightbarOnline"></span>
+               </div>
+               <span className="rightbarUsername"> Kavita P</span>
+            </li>
+            <li className="rightbarFriend">
+               <div className="rightbarProfileImgContainer">
+                  <img className="rightbarProfileImg" src="assets/person/3.jpeg" alt=""/>
+                  <span className="rightbarOnline"></span>
+               </div>
+               <span className="rightbarUsername"> Kavita P</span>
+            </li>
+            <li className="rightbarFriend">
+               <div className="rightbarProfileImgContainer">
+                  <img className="rightbarProfileImg" src="assets/person/3.jpeg" alt=""/>
+                  <span className="rightbarOnline"></span>
+               </div>
+               <span className="rightbarUsername"> Kavita P</span>
+            </li>
+            <li className="rightbarFriend">
+               <div className="rightbarProfileImgContainer">
+                  <img className="rightbarProfileImg" src="assets/person/3.jpeg" alt=""/>
+                  <span className="rightbarOnline"></span>
+               </div>
+               <span className="rightbarUsername"> Kavita P</span>
+            </li>
+            <li className="rightbarFriend">
+               <div className="rightbarProfileImgContainer">
+                  <img className="rightbarProfileImg" src="assets/person/3.jpeg" alt=""/>
+                  <span className="rightbarOnline"></span>
+               </div>
+               <span className="rightbarUsername"> Kavita P</span>
+            </li>
+          </ul>
+        </div>
     </div>
   )
 }
